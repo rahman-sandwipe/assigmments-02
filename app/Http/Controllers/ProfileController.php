509b0848c3d49201ab6b6d93b66ff009ca2c7b6c
M Rahman <ctgeye.news@gmail.com>
2024-12-14 15:10:53 +0600

    Laravel File Upload Learning

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request, $id){   
+        // variables and assign the values
+        $id = $request->id;
+        $name = 'Donald Trump';
+        $age = '75';
+        
+        // variables in $data variable as associative array with values
+        $data = [
+            'id'    => $id,
+            'name'  => $name,
+            'age'   => $age
+        ];
+        
+        //Set your cookie variables
+
+        // cookie set application -> cookie
+        $cookie_name = "access_token";
+        $cookie_value = "123-XYZ";
+        $cookie_expiry = time()+(160);
+        $cookie_path = "/";
+        $cookie_domain = $_SERVER['SERVER_NAME'];
+        $cookie_secure = false;
+        $cookie_httponly = true;
+        
+        $returnCookie=setcookie($cookie_name,$cookie_value,$cookie_expiry,$cookie_path,$cookie_domain,$cookie_secure,$cookie_httponly);
+        return response()->json($returnCookie, 200);
+    }
+}
